64be8ae3cfb57efda2eb7bc2811533b32fa6159b
Mojie <youjiefun@gmail.com>
2021-11-19 17:42:49 +0800

    feat(Object): deleteEmptyField

--- src/__tests__/object.spec.ts
+++ src/__tests__/object.spec.ts
@@ -3,11 +3,15 @@
  * @Date: 2021-11-17 17:29:20
  */
 
-import { objectToFormData } from '../object'
+import { objectToFormData, deleteEmptyField } from '../object'
 
 describe('Object Utils', () => {
   test('objectToFormData', () => {
     const formData = objectToFormData({ a: 1, b: 2 })
     expect(formData.get('a')).toEqual('1')
+  })
+  test('deleteEmptyField', () => {
+    const obj = { a: null, b: 2, c: 0, d: '', e: undefined }
+    expect(deleteEmptyField(obj)).toEqual({ b: 2, c: 0 })
   })
 })
--- src/object.ts
+++ src/object.ts
@@ -3,6 +3,8 @@
  * @Date: 2021-11-05 11:06:26
  */
 
+import { isEmpty, isObject } from './is'
+
 /**
  * 把 Object 对象转成 FormData 格式
  * @param obj 需要转换的Object对象
@@ -20,4 +22,21 @@ export const objectToFormData = (obj: object): FormData => {
     formData.append(key, (obj as IndexSign)[key])
   })
   return formData
+}
+
+/**
+ * 删除对象中的 ''、undefined、null
+ * @param obj 需要进行处理的对象
+ * @returns 返回删除''、undefined、null后的对象。如果不是object类型则返回原值
+ */
+export const deleteEmptyField = (obj: object) => {
+  if (isObject(obj)) {
+    Object.keys(obj).forEach((key) => {
+      const _obj = obj as AnyType
+      if (isEmpty(_obj[key])) {
+        delete _obj[key]
+      }
+    })
+  }
+  return obj
 }
