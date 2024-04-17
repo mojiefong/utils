dcf77014ac89ac4fbeee25ca42e7567081d06d9c
Mojie <mojiefong@gmail.com>
2024-04-17 16:48:56 +0800

    chore(array): delete groupBy

--- src/array.ts
+++ src/array.ts
@@ -25,45 +25,6 @@ export function toArray<T>(target?: Nullable<Arrayable<T>>): T[] {
 }
 
 /**
- * 根据key对一组对象进行分组
- * @param arr 需要进行分组的数组
- * @param property 指定key进行分组
- * @returns 返回组合后的对象
- * @category Array
- * @example
- * ``` typescript
- * groupBy(['one', 'two', 'three'], 'length') // { '3': ['one', 'two'], '5': ['three'] }
- * groupBy(
- *  [
- *    { branch: 'audi', model: 'q8', year: '2019' },
- *    { branch: 'audi', model: 'rs7', year: '2020' },
- *    { branch: 'ford', model: 'mustang', year: '2019' },
- *    { branch: 'ford', model: 'explorer', year: '2020' },
- *    { branch: 'bmw', model: 'x7', year: '2020' },
- *  ], 'branch'
- * )
- * // 返回
- * {
- *  audi: [
- *    { branch: 'audi', model: 'q8', year: '2019' },
- *    { branch: 'audi', model: 'rs7', year: '2020' },
- *  ],
- *  bmw: [{ branch: 'bmw', model: 'x7', year: '2020' }],
- *  ford: [
- *    { branch: 'ford', model: 'mustang', year: '2019' },
- *    { branch: 'ford', model: 'explorer', year: '2020' },
- *  ]
- * }
- * ```
- */
-export function groupBy<T>(arr: T[], fn: string | (<T>(val: T, index?: number, arr?: T[]) => T)): T {
-  return arr.map(isString(fn) ? (val: any) => val[fn] : fn).reduce((acc, val, i) => {
-    acc[val] = (acc[val] || []).concat(arr[i])
-    return acc
-  }, {})
-}
-
-/**
  * 获取数组的第一个元素
  * @param arr 需要获取的数组
  * @category Array
